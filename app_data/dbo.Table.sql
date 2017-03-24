0d56ae970f3697b407ee075b7be1185df1600a9b
gh26300 <gh26300@gmail.com>
2017-03-24 13:38:24 +0800

    com

--- app_data/dbo.Table.sql
+++ app_data/dbo.Table.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Table]
 (
 	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [ddesc] NVARCHAR(50) NULL, 
     [nameZh] NVARCHAR(50) NULL, 
+    [ddesc] NVARCHAR(50) NULL, 
     [departureZh] NVARCHAR(50) NULL, 
     [destinationZh] NVARCHAR(50) NULL, 
     [CreateTime] SMALLDATETIME NULL
