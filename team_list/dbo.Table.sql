e7e1710d584225326cdb88d03565739024055ad5
gh26300 <gh26300@gmail.com>
2017-03-24 11:34:57 +0800

    DB CREATE

---
+++ team_list/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [ddesc] NVARCHAR(50) NULL, 
+    [nameZh] NVARCHAR(50) NULL, 
+    [departureZh] NVARCHAR(50) NULL, 
+    [destinationZh] NVARCHAR(50) NULL, 
+    [CreateTime] SMALLDATETIME NULL
+)
