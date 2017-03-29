db7699a4ececfc6ff817f899b23e7da06cb7c10a
gh26300 <gh26300@gmail.com>
2017-03-29 13:51:58 +0800

    nope

--- app_data/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [nameZh] NVARCHAR(50) NULL, 
-    [ddesc] NVARCHAR(50) NULL, 
-    [departureZh] NVARCHAR(50) NULL, 
-    [destinationZh] NVARCHAR(50) NULL, 
-    [CreateTime] SMALLDATETIME NULL
-)
